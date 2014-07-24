cef34d30dfc805cff964eea46b28bf25ab44c2bc
Neil Wang <neil.wang@10data.com>
2014-07-24 15:29:51 +0800

    Prolog implementation

---
+++ prolog.lisp
@@ -0,0 +1,77 @@
+(defun clause-head (clause)
+  (first clause))
+
+(defun clause-body (clause)
+  (rest clause))
+
+(defun get-clauses (pred)
+  (get pred 'clauses))
+
+(defun predicate (relation)
+  (first relation))
+
+(defvar *db-predicates* nil
+  "A list of all predicates stored in the database")
+
+(defmacro <- (&rest clause)
+  "Add a clause to the database"
+  `(add-clause ',clause))
+
+(defun add-clause (clause)
+  "Add a clause to the database, indexed by head's predicate"
+  (let ((pred (predicate (clause-head clause))))
+    (assert (and (symbolp pred) (not (variable-p pred))))
+    (pushnew pred *db-predicates*)
+    (setf (get pred 'clause)
+	  (nconc (get-clauses pred) (list clause)))
+    pred))
+
+(defun clear-db ()
+  "Remove all clauses (for all predicates) from the database"
+  (mapc #'clear-predicate *db-predicates*))
+
+(defun clear-predicate (predicate)
+  "Remove the clauses for a single predicate"
+  (setf (get predicate 'clauses) nil))
+
+(defun prove (goal bindings)
+  "Return a list of possible solutions to goal"
+  (mapcan #'(lambda (clause)
+	      (let ((new-clause (rename-variables clause)))
+		(prove-all (clause-body new-clause)
+			   (unify goal (clause-head new-clause) bindings))))
+	  (get-clauses (predicate goal))))
+
+(defun prove-all (goals bindings)
+  "Return a list of solutions to the conjunction of goals"
+  (cond ((eq bindings fail) fail)
+	((null goals) (list bindings))
+	(t (mapcan #'(lambda (goal-solution)
+		       (prove-all (rest goals) goal-solution))
+		   (prove (first goals) bindings)))))
+
+(defun rename-variables (x)
+  "Replace all variables in x with new ones"
+  (sublis (mapcar #'(lambda (var)
+		      (cons var (gensym (string var))))
+		  (variables-in x))
+	  x))
+
+(defun variables-in (expr)
+  "Return a list of all the variables in expr"
+  (unique-find-anywhere-if #'variable-p expr))
+
+(defun unique-find-anywhere-if (predicate tree &optional found-so-far)
+  "Return a list of leaves of tree satisfying predicate, with duplicates removed"
+  (if (atom tree)
+      (if (funcall predicate tree)
+	  (adjoin tree found-so-far)
+	  found-so-far)
+      (unique-find-anywhere-if predicate
+			       (first tree)
+			       (unique-find-anywhere-if predicate
+							(rest tree)
+							found-so-far))))
+
+(defmacro ?- (&rest goals)
+  `(prove-all ',goals no-bindings))
